92f5e851dfe7aaaff39cb1e7a809fe969622052f
Gal Rogozinski <galrogogit@gmail.com>
2018-05-30 15:43:22 +0300

    Mock entryPoint (#7)
    * Use mockito for entry point selector impl
    
    * mock milestone tracker can recieve parameters
    
    * remove todos
    
    * don't use an empty array to initialize hash

--- src/test/java/com/iota/iri/service/tipselection/impl/EntryPointSelectorImplTest.java
+++ src/test/java/com/iota/iri/service/tipselection/impl/EntryPointSelectorImplTest.java
@@ -1,80 +1,63 @@
 package com.iota.iri.service.tipselection.impl;
 
-import com.iota.iri.LedgerValidator;
 import com.iota.iri.Milestone;
-import com.iota.iri.Snapshot;
-import com.iota.iri.TransactionValidator;
-import com.iota.iri.conf.Configuration;
-import com.iota.iri.controllers.TipsViewModel;
-import com.iota.iri.controllers.TransactionViewModel;
+import com.iota.iri.hash.SpongeFactory;
 import com.iota.iri.model.Hash;
-import com.iota.iri.network.TransactionRequester;
+import com.iota.iri.model.IntegerIndex;
 import com.iota.iri.service.tipselection.EntryPointSelector;
+import com.iota.iri.storage.Indexable;
+import com.iota.iri.storage.Persistable;
 import com.iota.iri.storage.Tangle;
-import com.iota.iri.storage.rocksDB.RocksDBPersistenceProvider;
-import com.iota.iri.zmq.MessageQ;
-import org.junit.AfterClass;
+import com.iota.iri.utils.Pair;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import static com.iota.iri.controllers.TransactionViewModelTest.getRandomTransactionHash;
-import static com.iota.iri.controllers.TransactionViewModelTest.getRandomTransactionWithTrunkAndBranchValidBundle;
-
+@RunWith(MockitoJUnitRunner.class)
 public class EntryPointSelectorImplTest {
 
-    private static final TemporaryFolder dbFolder = new TemporaryFolder();
-    private static final TemporaryFolder logFolder = new TemporaryFolder();
-    private static Tangle tangle;
-    private static EntryPointSelector entryPointSelector;
-
-    @AfterClass
-    public static void tearDown() throws Exception {
-        tangle.shutdown();
-        dbFolder.delete();
-    }
-
-    @BeforeClass
-    public static void setUp() throws Exception {
-        tangle = new Tangle();
-        dbFolder.create();
-        logFolder.create();
-        tangle.addPersistenceProvider(new RocksDBPersistenceProvider(dbFolder.getRoot().getAbsolutePath(), logFolder
-                .getRoot().getAbsolutePath(), 1000));
-        tangle.init();
-        MessageQ messageQ = new MessageQ(0, null, 1, false);
-
-        TipsViewModel tipsViewModel = new TipsViewModel();
-        TransactionRequester transactionRequester = new TransactionRequester(tangle, messageQ);
-        TransactionValidator transactionValidator = new TransactionValidator(tangle, tipsViewModel, transactionRequester,
-                messageQ, Long.parseLong(Configuration.GLOBAL_SNAPSHOT_TIME));
-        int milestoneStartIndex = Integer.parseInt(Configuration.MAINNET_MILESTONE_START_INDEX);
-        int numOfKeysInMilestone = Integer.parseInt(Configuration.MAINNET_NUM_KEYS_IN_MILESTONE);
-        Milestone milestone = new Milestone(tangle, Hash.NULL_HASH, Snapshot.init(
-                Configuration.MAINNET_SNAPSHOT_FILE, Configuration.MAINNET_SNAPSHOT_SIG_FILE, false).clone(),
-                transactionValidator, false, messageQ, numOfKeysInMilestone,
-                milestoneStartIndex, true);
-
-        entryPointSelector = new EntryPointSelectorImpl(tangle, milestone, false, milestoneStartIndex);
-    }
-
+    @Mock
+    private Milestone milestone;
+    @Mock
+    private Tangle tangle;
 
     @Test
-    public void testEntryPoint() throws Exception {
-        //build a small tangle - 1,2,3,4 point to  transaction
-        TransactionViewModel transaction;
-        transaction = new TransactionViewModel(getRandomTransactionWithTrunkAndBranchValidBundle(Hash.NULL_HASH,
-                Hash.NULL_HASH), getRandomTransactionHash());
+    public void testEntryPointWithTangleData() throws Exception {
+        Hash milestoneHash = Hash.calculate(SpongeFactory.Mode.CURLP81, new int[] {1});
+        mockTangleBehavior(milestoneHash);
+        mockMilestoneTrackerBehavior(0, Hash.NULL_HASH);
 
-        transaction.store(tangle);
+        EntryPointSelector entryPointSelector = new EntryPointSelectorImpl(tangle, milestone, false, 0);
+        Hash entryPoint = entryPointSelector.getEntryPoint(10);
 
-        //change transaction to pass validation:
-        transaction.updateSolid(true);
-        Hash entryPointHash = entryPointSelector.getEntryPoint(10);
-        Assert.assertNotNull(entryPointHash);
+        Assert.assertEquals(milestoneHash, entryPoint);
     }
 
+    @Test
+    public void testEntryPointWithoutTangleData() throws Exception {
+        mockMilestoneTrackerBehavior(0, Hash.NULL_HASH);
+
+        EntryPointSelector entryPointSelector = new EntryPointSelectorImpl(tangle, milestone, false, 0);
+        Hash entryPoint = entryPointSelector.getEntryPoint(10);
+
+        Assert.assertEquals(Hash.NULL_HASH, entryPoint);
+    }
+
+
+    private void mockMilestoneTrackerBehavior(int latestSolidSubtangleMilestoneIndex, Hash latestSolidSubtangleMilestone) {
+        milestone.latestSolidSubtangleMilestoneIndex = latestSolidSubtangleMilestoneIndex;
+        milestone.latestSolidSubtangleMilestone = latestSolidSubtangleMilestone;
+    }
+
+    private void mockTangleBehavior(Hash milestoneModelHash) throws Exception {
+        com.iota.iri.model.Milestone milestoneModel = new com.iota.iri.model.Milestone();
+        milestoneModel.index = new IntegerIndex(0);
+        milestoneModel.hash = milestoneModelHash;
+        Pair<Indexable, Persistable> indexMilestoneModel = new Pair<>(new IntegerIndex(0), milestoneModel);
+        Mockito.when(tangle.getFirst(com.iota.iri.model.Milestone.class, IntegerIndex.class))
+                .thenReturn(indexMilestoneModel);
+    }
 }
